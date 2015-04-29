eec14cdc4fdd4fa21ada3849ba6d8d4268cbf365
Ken Liao <kenliao94@gmail.com>
2015-04-28 20:01:00 -0400

    Add user_input.pl for reference

---
+++ perl_snippet/user_input.pl
@@ -0,0 +1,3 @@
+# <STDIN> will read the next complete line from standard input (up to the first newline character)and use that string as the value of <STDIN> 
+$line = <STDIN>;
+print $line; # print what you enter before you press ENTER 
