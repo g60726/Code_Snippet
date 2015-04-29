4de6b4285ff0b94476790ee44de04f353c62107d
Ken Liao <kenliao94@gmail.com>
2015-04-28 20:23:53 -0400

    change loop.pl

---
+++ perl_snippet/loop.pl
@@ -0,0 +1,20 @@
+#using loop
+
+#while loop 
+$count = 0;
+while($count < 10){
+	$count += 1;
+	print "The count is: $count \n";
+}
+
+#concept of undef, a variable no need to declared before it is used, if it is used as number, then it is assumed to be 0
+# else if it is used as string it is assumed as empty string 
+# to check if a variable is defined, use defined(), <STDIN> operator will return undef if it hits the end-of-file 
+if(defined($name)){
+	print "the variable is defined\n";
+}
+else{
+	print "the variable is not defined\n";
+}
+
+
--- perl_snippet/user_input.pl
+++ perl_snippet/user_input.pl
@@ -1,3 +1,9 @@
 # <STDIN> will read the next complete line from standard input (up to the first newline character)and use that string as the value of <STDIN> 
 $line = <STDIN>;
-print $line; # print what you enter before you press ENTER 
+print $line; # print what you enter before you press ENTER 	
+
+#using chomp to remove the trailing newline character, however it only remove one newline character 
+print "used chomp\n";
+chomp($line);
+print $line."end"; #there is no space between the user entered and 'end'
+
