5201a96be07be7c9a3178d782528cbf7ad04e6dc
Ken Liao <kenliao94@gmail.com>
2015-05-01 15:08:00 -0400

    add termin

---
+++ sql_snippet/basic.sql
@@ -0,0 +1,11 @@
+# cheat sheet
+#http://en.wikibooks.org/wiki/MySQL/CheatSheet
+
+# listing databases on terminal 
+SHOW DATABASES; 
+
+#create database
+CREATE DATABASE <dbname> [CHARACTER SET charset] [COLLATE collation]
+
+#create Table
+CREATE TABLE <table> (<field> <type>, <field> <type>, ...)
