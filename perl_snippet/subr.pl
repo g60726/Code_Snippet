a9262e71dd142989217dee21f098a1317b26fa9f
Ken Liao <kenliao94@gmail.com>
2015-05-04 14:36:13 -0400

    finish subroutine exercise

--- perl_snippet/subr.pl
+++ perl_snippet/subr.pl
@@ -33,7 +33,55 @@ print $sum."\n";
 #the list to the subroutine, store in @_ array to access the first arugment, use $_[0]
 
 sub multiply{
-	$_[0] * $_[1];  #first argument multiply to the second argument 
+	#check if the parameter num is correct
+	if(@_ != 2){ 
+		print "Please Pass in two numbers\n";
+	}
+	else{
+		$_[0] * $_[1];  #first argument multiply to the second argument 
+	}
 }
+
+#--------------------------------------------------------------
+
+# use strict or use warning at the top to turn on warning during compilation time 
+
+#write a subrountine that finds the sum of array, if the array is less than 4 elements, return -1
+
+sub sum_of_array {
+  if(@_ < 4){
+  	print "Please Enter more than 4 elements\n";
+    return -1;  
+  }
+
+  else{
+  	$sum = 0; 
+  	foreach(@_){
+      $sum += $_;
+  	}
+  	$sum;
+  }
+}
+
+print "\nTesting sum_of_array\n";
+@arr1 = (1,2,3,4,5);
+print &sum_of_array(@arr1)."\n";
+
+#----------------------------------------------------------------
+
+#if you use strict, if you declare global variable within a function, you will encounter a compilation error
+# if you want to persist and local declare as state 
+use 5.010;
+sub use_state{
+	state $n = 0;
+	$n += 1;
+	print "\nusing state: $n\n";
+}
+
+foreach(1..3){
+	&use_state;
+}
+
+
 
 
