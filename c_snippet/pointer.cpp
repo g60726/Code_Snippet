5168d820d01d12d74c515a7a93cafefc49c2b527
kenliao94 <kenliao94@gmail.com>
2015-06-30 23:41:29 -0400

    Add more C++ reference

--- c_snippet/pointer.cpp
+++ c_snippet/pointer.cpp
@@ -18,6 +18,33 @@ int main(){
 	//free the memory
 	delete p_number; //only free up the memory allocated from new
 
+  /*
+	* Struct 
+	*/
+	//struct definition
+	struct Node
+	{
+		int number; 
+	}; 
+	//initiate the note 
+
+	Node example_node;
+	example_node.number = 5; 
+	cout << "Number (within the node) is : " << example_node.number <<endl; 
+	
+	//pointer to a struct
+
+	Node* p_node;
+	p_node = &example_node;
+	p_node->number = 6;
+	cout << "Number (within the node referred by pointer) is : " << example_node.number <<endl;
+
+	//pointer created by using new
+	Node* p_node2 = new Node;
+	p_node2->number = 12;
+	cout << "Number in p_node2 is: " << p_node2->number << endl;
+	delete p_node2;
+
+
 	return 1;
- 
 }
